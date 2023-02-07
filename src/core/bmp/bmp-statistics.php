23f4f56eb5833405c177cad903da97cce9a45751
Leonid Vinikov <czf.leo123@gmail.com>
2023-02-07 13:21:47 +0200

    Fix: Core/BMP - Move `get_bmp_colors` to `bmp-colors.php'

--- src/core/bmp/bmp-statistics.php
+++ src/core/bmp/bmp-statistics.php
@@ -5,83 +5,9 @@
 
 namespace Core\BMP;
 
-use Decoders\RLE4_Decoder;
-use Decoders\RLE8_Decoder;
-use Enums\E_Bits_Per_Pixel;
-use Enums\E_Compressions;
-use Exception;
 use File_Readers\Bitmap_File_Reader;
 
 const MAX_COLORS_OCCURRENCE = 20;
-
-/**
- * Function get_bmp_colors().
- *
- * Return array of all colors for given pixels data
- *
- * @param Bitmap_File_Reader $file_reader
- *
- * @return array
- */
-function get_bmp_colors( Bitmap_File_Reader $file_reader): array {
-	/**
-	 * @var E_Bits_Per_Pixel $bits_per_pixel
-	 */
-	$bits_per_pixel = $file_reader['bits_per_pixel'];
-
-	switch ( $bits_per_pixel->get_value() ) {
-		// Check research in `<project-root>/tests/1bpp/1bpp-test.php` for more info.
-		case E_Bits_Per_Pixel::E_BPP_1:
-			$result = get_colors_binary_1bpp( $file_reader );
-			break;
-
-		case E_Bits_Per_Pixel::E_BPP_4:
-			if ( E_Compressions::E_COMPRESSIONS_BI_RLE4 === (string) $file_reader['compression'] ){
-				$decoder = new RLE4_Decoder(
-					$file_reader['width'],
-					$file_reader['height'],
-					$file_reader['body'],
-					$file_reader['body_length']
-				);
-
-				$result = get_colors_simple( $file_reader, $decoder->decode()->get_data() );
-
-				break;
-			}
-
-			$result = get_colors_binary_4bpp( $file_reader );
-
-			break;
-
-		case E_Bits_Per_Pixel::E_BPP_8:
-			if ( E_Compressions::E_COMPRESSIONS_BI_RLE8 === (string) $file_reader['compression'] ){
-				$decoder = new RLE8_Decoder(
-					$file_reader['width'],
-					$file_reader['height'],
-					$file_reader['body'],
-					$file_reader['body_length']
-				);
-
-				$result = get_colors_simple( $file_reader, $decoder->decode()->get_data() );
-
-				break;
-			}
-
-			$result = get_colors_byte_8bpp( $file_reader );
-
-			break;
-
-		case E_Bits_Per_Pixel::E_BPP_24:
-		case E_Bits_Per_Pixel::E_BPP_32:
-			$result = get_colors_rgb_higher_then_24bpp( $file_reader );
-			break;
-
-		default:
-			throw new Exception( "Unsupported bits per pixel value: '$bits_per_pixel'." );
-	}
-
-	return $result;
-}
 
 /**
  * Function get_bmp_statistics().
